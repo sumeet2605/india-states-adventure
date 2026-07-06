076e6daff7924e2a18c8c918c33cb79167d32f2a
sumeet2605 <75994952+sumeet2605@users.noreply.github.com>
2026-07-06 13:37:56 +0530

    Add quiz bank aggregator

---
+++ src/data/isoQuizAll.ts
@@ -0,0 +1,6 @@
+import { isoMissionQuiz } from './isoMissionQuiz';
+import { isoMissionQuizExtraA } from './isoMissionQuizExtraA';
+import { isoMissionQuizExtraB } from './isoMissionQuizExtraB';
+import { isoMissionQuizExtraC } from './isoMissionQuizExtraC';
+
+export const isoQuizAll = isoMissionQuiz.concat(isoMissionQuizExtraA, isoMissionQuizExtraB, isoMissionQuizExtraC);
